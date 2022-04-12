de7cbd28fcff9bee5563263c479c6bade60846a1
Emmanuel-Odero <emmanuelodero808@gmail.com>
2022-04-12 21:31:53 +0300

    created the delete button and ensured it is deleting

--- process.php
+++ process.php
@@ -6,4 +6,9 @@ if (isset($_POST['save'])) {
     $location = $_POST['location'];
     $mysqli->query("INSERT INTO data (name, location) VALUES('$name', '$location')") or die($mysqli->error);
 }
+if (isset($_GET['delete'])) {
+    # code...
+    $id=$_GET['delete'];
+    $mysqli->query("DELETE FROM data WHERE id=$id")or die($mysqli->error());
+}
 ?>
